11c1a6eede4de9fa8ec0411d8e81dbf328ef164f
liu <remnantsaint@163.com>
2024-09-07 16:43:28 +0800

    1

---
+++ valaxy.config.ts
@@ -0,0 +1,51 @@
+import { defineValaxyConfig } from 'valaxy'
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '西柿',
+      cloud: {
+        enable: true,
+      },
+    },
+
+    pages: [
+      {
+        name: '我的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-genderless-line',
+        color: 'dodgerblue',
+      },
+      {
+        name: '喜欢的女孩子',
+        url: '/girls/',
+        icon: 'i-ri-women-line',
+        color: 'hotpink',
+      },
+    ],
+
+    footer: {
+      since: 2016,
+      beian: {
+        enable: true,
+        icp: '萌ICP备20231227号',
+      },
+    },
+  },
+
+  unocss: { safelist },
+})
