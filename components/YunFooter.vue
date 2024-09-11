b4cbbed674a3b0564fb755838b39e4e2b74c9a08
remnantsaint <remnantsaint163.com>
2024-09-11 14:14:51 +0800

    busuanzi

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,12 +1,17 @@
-<!-- 新建 components/YunFooter.vue -->
 <script lang="ts" setup>
+import { useScriptTag } from '@vueuse/core'
 import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
+
+useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
 </script>
+
 <template>
   <YunFooter>
     <div class="copyright"><a href="https://icp.gov.moe/?keyword=20231227" target="_blank">
         萌ICP备20231227号</a>
     </div>
+    <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
+    <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
     <VCLiveTime start-time="2022-12-27">
       <template #live-time-before>
         <span>本站已运行</span>
